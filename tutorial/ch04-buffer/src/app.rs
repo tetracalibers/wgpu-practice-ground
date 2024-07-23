ee309b0c9e51321a855582d1b8e867223048c8e2
tetracalibers <tomix05may@gmail.com>
2024-07-24 07:12:00 +0900

    windowタイトルを変更

--- tutorial/ch04-buffer/src/app.rs
+++ tutorial/ch04-buffer/src/app.rs
@@ -16,7 +16,7 @@ pub struct Application<'w> {
 impl<'w> ApplicationHandler for Application<'w> {
   fn resumed(&mut self, event_loop: &ActiveEventLoop) {
     let window_attributes =
-      Window::default_attributes().with_title("Tutorial 03 - Pipeline");
+      Window::default_attributes().with_title("Tutorial 04 - Buffer");
 
     let window = event_loop
       .create_window(window_attributes)
