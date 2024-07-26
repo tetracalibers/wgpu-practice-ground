9e9bb63ae8c62d448998aa47afea00361165fb56
tetracalibers <tomix05may@gmail.com>
2024-07-27 08:18:06 +0900

    instanceの作成

--- tutorial/life-game/src/state.rs
+++ tutorial/life-game/src/state.rs
@@ -10,10 +10,21 @@ impl State {
   pub async fn new(window: Window) -> Self {
     let window = Arc::new(window);
 
+    let instance = Self::create_gpu_instance();
+
     Self { window }
   }
 
   pub fn window(&self) -> &Window {
     &self.window
+  }
+
+  fn create_gpu_instance() -> wgpu::Instance {
+    wgpu::Instance::new(wgpu::InstanceDescriptor {
+      // Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+      // allとすることですべてのバックエンドから必要なものを選択してくれる
+      backends: wgpu::Backends::all(),
+      ..Default::default()
+    })
   }
 }
