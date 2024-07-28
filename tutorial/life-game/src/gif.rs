2c80689c18d84a364bc6c6b5e27f25f7b09ddc62
tetracalibers <tomix05may@gmail.com>
2024-07-28 20:09:05 +0900

    補足コメント追加

--- tutorial/life-game/src/gif.rs
+++ tutorial/life-game/src/gif.rs
@@ -20,6 +20,10 @@ pub async fn export() {
     .await
     .unwrap();
 
+  //
+  // レンダリング先のテクスチャ
+  // surface.get_current_texture()を使って描画するテクスチャを取得する代わりに、テクスチャを自分で作成する
+  //
   let texture_size = 512;
   let texture_desc = wgpu::TextureDescriptor {
     size: wgpu::Extent3d {
@@ -31,6 +35,8 @@ pub async fn export() {
     sample_count: 1,
     dimension: wgpu::TextureDimension::D2,
     format: wgpu::TextureFormat::Rgba8UnormSrgb,
+    // TextureUsages::RENDER_ATTACHMENTを使って、wgpuがテクスチャにレンダリングできるようにする
+    // TextureUsages::COPY_SRCはテクスチャからデータを取り出してファイルに保存できるようにするため
     usage: wgpu::TextureUsages::COPY_SRC
       | wgpu::TextureUsages::RENDER_ATTACHMENT,
     label: None,
@@ -39,16 +45,27 @@ pub async fn export() {
   let texture = device.create_texture(&texture_desc);
   let texture_view = texture.create_view(&Default::default());
 
+  //
+  // wgpuはテクスチャからバッファへのコピーがCOPY_BYTES_PER_ROW_ALIGNMENTを使用して整列されることを要求する
+  // このため、padded_bytes_per_rowとunpadded_bytes_per_rowの両方を保存する必要がある
+  //
   let pixel_size = mem::size_of::<[u8; 4]>() as u32;
   let align = wgpu::COPY_BYTES_PER_ROW_ALIGNMENT;
   let unpadded_bytes_per_row = pixel_size * texture_size;
   let padding = (align - unpadded_bytes_per_row % align) % align;
   let padded_bytes_per_row = unpadded_bytes_per_row + padding;
 
+  //
+  // 出力をコピーするバッファを作成する
+  //
+  // 最終的に、テクスチャからバッファにデータをコピーしてファイルに保存する
+  // データを保存するためには、十分な大きさのバッファが必要
+  //
   let buffer_size =
     (padded_bytes_per_row * texture_size) as wgpu::BufferAddress;
   let buffer_desc = wgpu::BufferDescriptor {
     size: buffer_size,
+    // BufferUsages::MAP_READは、wpguにこのバッファをCPUから読み込みたいことを伝える
     usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
     label: Some("Output Buffer"),
     mapped_at_creation: false,
@@ -58,15 +75,18 @@ pub async fn export() {
   let mut renderer =
     Renderer::new(&device, wgpu::TextureFormat::Rgba8UnormSrgb).await;
 
+  // フレームをレンダリングし、そのフレームをVec<u8>にコピーする
   let mut frames = Vec::new();
 
   for _ in 0..SCENE_COUNT {
     let mut encoder = device
       .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
 
+    // テクスチャに描画する
     renderer.update();
     renderer.draw(&mut encoder, &texture_view);
 
+    // テクスチャの内容をバッファにコピーする
     encoder.copy_texture_to_buffer(
       wgpu::ImageCopyTexture {
         texture: &texture,
@@ -87,13 +107,14 @@ pub async fn export() {
 
     queue.submit(iter::once(encoder.finish()));
 
-    // Create the map request
+    //
+    // バッファからデータを取り出すには、まずバッファをマップし、バッファビューを取得して、それを&[u8]のように扱う必要がある
+    //
     let buffer_slice = output_buffer.slice(..);
     let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
     buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
       tx.send(result).unwrap();
     });
-    // wait for the GPU to finish
     device.poll(wgpu::Maintain::Wait);
 
     match rx.receive().await {
