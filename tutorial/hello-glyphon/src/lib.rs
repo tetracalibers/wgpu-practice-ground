606661590a9b0937ad37665b9d7e5bf513f84a74
tetracalibers <tomix05may@gmail.com>
2024-08-06 07:12:02 +0900

    windowの立ち上げ

--- tutorial/hello-glyphon/src/lib.rs
+++ tutorial/hello-glyphon/src/lib.rs
@@ -1,9 +1,18 @@
+mod app;
+mod state;
+
 use std::error::Error;
+
+use app::Application;
+use winit::event_loop::EventLoop;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
   env_logger::init();
 
-  println!("glyphon: Hello World");
+  let event_loop = EventLoop::builder().build()?;
+  let mut app = Application::default();
+
+  event_loop.run_app(&mut app)?;
 
   Ok(())
 }
