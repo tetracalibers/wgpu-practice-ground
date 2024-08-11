d08c3910f2e89d184c86b8ae0b4fcfaca2bc3f1a
tetracalibers <tomix05may@gmail.com>
2024-08-11 15:53:07 +0900

    rusttypeで複数文字をビットマップ化する実験

--- prototype/text-renderer/src/lib.rs
+++ prototype/text-renderer/src/lib.rs
@@ -104,10 +104,8 @@ pub fn try_swash() -> Result<(), Box<dyn Error>> {
 }
 
 pub fn try_rusttype() -> Result<(), Box<dyn Error>> {
-  use rusttype::Font;
-
-  const CHARACTER: char = 'Q';
-  const SIZE: f32 = 200.0;
+  use image::{DynamicImage, Rgba};
+  use rusttype::{point, Font, Scale};
 
   let font_path = "./font/Poiret_One/PoiretOne-Regular.ttf";
   let font_data = std::fs::read(font_path)?;
@@ -115,37 +113,53 @@ pub fn try_rusttype() -> Result<(), Box<dyn Error>> {
   let font = Font::try_from_bytes(font_data.as_slice())
     .expect("error constructing a Font from bytes");
 
-  let glyph = font
-    .glyph(CHARACTER)
-    .scaled(rusttype::Scale::uniform(SIZE))
-    .positioned(rusttype::point(0.0, 0.0));
-  let (height, width) = if let Some(rect) = glyph.pixel_bounding_box() {
-    (rect.height(), rect.width())
-  } else {
-    (0, 0)
+  // The font size to use
+  let scale = Scale::uniform(32.0);
+
+  // The text to render
+  let text = "This is RustType rendered into a png!";
+
+  // Use a dark red colour
+  let colour = (150, 0, 0);
+
+  let v_metrics = font.v_metrics(scale);
+
+  // layout the glyphs in a line with 20 pixels padding
+  let glyphs: Vec<_> =
+    font.layout(text, scale, point(20.0, 20.0 + v_metrics.ascent)).collect();
+
+  // work out the layout size
+  let glyphs_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
+  let glyphs_width = {
+    let min_x =
+      glyphs.first().map(|g| g.pixel_bounding_box().unwrap().min.x).unwrap();
+    let max_x =
+      glyphs.last().map(|g| g.pixel_bounding_box().unwrap().max.x).unwrap();
+    (max_x - min_x) as u32
   };
 
-  let mut bitmap = vec![0u8; (width * height) as usize];
-  glyph.draw(|x, y, v| {
-    bitmap[(x as usize) + (y as usize) * width as usize] = (v * 255.0) as u8;
-  });
+  // Create a new rgba image with some padding
+  let mut image =
+    DynamicImage::new_rgba8(glyphs_width + 40, glyphs_height + 40).to_rgba8();
 
-  let out_path = std::path::Path::new(r"./export/glyph-rusttype.png");
-  let out_file = std::fs::File::create(out_path).unwrap();
-  let ref mut w = std::io::BufWriter::new(out_file);
+  // Loop through the glyphs in the text, positing each one on a line
+  for glyph in glyphs {
+    if let Some(bounding_box) = glyph.pixel_bounding_box() {
+      // Draw the glyph into the image per-pixel by using the draw closure
+      glyph.draw(|x, y, v| {
+        image.put_pixel(
+          // Offset the position by the glyph bounding box
+          x + bounding_box.min.x as u32,
+          y + bounding_box.min.y as u32,
+          // Turn the coverage into an alpha value
+          Rgba([colour.0, colour.1, colour.2, (v * 255.0) as u8]),
+        )
+      });
+    }
+  }
 
-  let mut encoder = png::Encoder::new(w, width as u32, height as u32);
-  encoder.set_color(png::ColorType::Grayscale);
-
-  println!(
-    "width: {}, height: {}, bitmap.len(): {}",
-    width,
-    height,
-    bitmap.len()
-  );
-
-  let mut writer = encoder.write_header().unwrap();
-  writer.write_image_data(&bitmap).unwrap();
+  // Save the image to a png file
+  image.save("./export/glyphs-rusttype.png").unwrap();
 
   Ok(())
 }
@@ -155,7 +169,7 @@ pub fn proto() -> Result<(), Box<dyn Error>> {
   use std::collections::HashMap;
   use ttf_parser as ttf;
 
-  const ATLAS_FONT_SIZE: u16 = 96;
+  const ATLAS_FONT_SIZE: u16 = 32;
   const ATLAS_GAP: u16 = 4;
 
   let font_path = "./font/Sankofa_Display/SankofaDisplay-Regular.ttf";
@@ -259,7 +273,7 @@ pub fn proto() -> Result<(), Box<dyn Error>> {
   // let atlas_size = ceil_pow2(atlas_width).max(ceil_pow2(atlas_height)) as i32;
 
   // TODO: 適切なサイズを算出するロジックを考える
-  let atlas_size = 4096;
+  let atlas_size = 1024;
 
   let mut atlas = etagere::AtlasAllocator::new(size2(atlas_size, atlas_size));
 
@@ -269,8 +283,8 @@ pub fn proto() -> Result<(), Box<dyn Error>> {
     .map(|(w, h)| atlas.allocate(size2(*w as i32, *h as i32)).unwrap())
     .collect::<Vec<_>>();
 
-  // let mut atlas_svg = std::fs::File::create("export/font-atlas.svg")?;
-  // atlas.dump_svg(&mut atlas_svg)?;
+  let mut atlas_svg = std::fs::File::create("export/font-atlas.svg")?;
+  atlas.dump_svg(&mut atlas_svg)?;
 
   let atlas_positions = allocations
     .iter()
