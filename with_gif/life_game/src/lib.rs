ea7c4bc774d6618b3bc2f03e291c9cf735d9c89c
tetracalibers <tomix05may@gmail.com>
2024-10-25 06:39:51 +0900

    create with_gif/life_game

---
+++ with_gif/life_game/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn run() -> Result<(), Box<dyn Error>> {
+  env_logger::init();
+
+  Ok(())
+}
+
+pub async fn export_gif() -> Result<(), Box<dyn Error>> {
+  env_logger::init();
+
+  Ok(())
+}
