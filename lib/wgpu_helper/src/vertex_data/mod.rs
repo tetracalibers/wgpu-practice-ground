a9e9ea65c01d1c9c9fac41dd97dfc577faddd2c3
tetracalibers <tomix05may@gmail.com>
2024-10-27 11:20:14 +0900

    陰影のないオブジェクト群を描画するサンプル

--- lib/wgpu_helper/src/vertex_data/mod.rs
+++ lib/wgpu_helper/src/vertex_data/mod.rs
@@ -2,3 +2,10 @@ pub mod cube;
 pub mod cylinder;
 pub mod sphere;
 pub mod torus;
+
+pub mod prelude {
+  pub use super::cube::*;
+  pub use super::cylinder::*;
+  pub use super::sphere::*;
+  pub use super::torus::*;
+}
--- practice/instanced_cube_sphere_torus/src/lib.rs
+++ practice/instanced_cube_sphere_torus/src/lib.rs
@@ -1,7 +1,39 @@
+mod instance_defs;
+
 use std::error::Error;
+
+use bytemuck::cast_slice;
+use cgmath::{Matrix4, Point3, Vector3};
+use instance_defs::{Matrices, Shapes, Vertex};
+use wgpu::util::DeviceExt;
+use wgpu_helper::context as helper_util;
+use wgpu_helper::context::WgpuContext;
+use wgpu_helper::framework::with_gif::{App, Gif, Render, RenderTarget};
+use wgpu_helper::transforms as wt;
+use winit::dpi::PhysicalSize;
+
+const NUM_CUBES: u32 = 50;
+const NUM_SPHERES: u32 = 50;
+const NUM_TORI: u32 = 50;
+
+fn setup(animation_speed: f32) -> Initial {
+  Initial {
+    camera_position: Point3::new(8., 8., 16.),
+    look_direction: Point3::new(0., 0., 0.),
+    up_direction: Vector3::unit_y(),
+
+    animation_speed,
+  }
+}
 
 pub fn run() -> Result<(), Box<dyn Error>> {
   env_logger::init();
+
+  let initial = setup(1.0);
+
+  let mut app: App<State> =
+    App::new("instanced_cube_sphere_torus", initial).with_msaa();
+  app.run()?;
 
   Ok(())
 }
@@ -9,5 +41,316 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 pub async fn export_gif() -> Result<(), Box<dyn Error>> {
   env_logger::init();
 
+  let initial = setup(1.0);
+
+  let mut gif = Gif::<State>::new(1024, initial, true).await;
+  gif.export("export/instanced_cube_sphere_torus.gif", 50, 1).await?;
+
   Ok(())
+}
+
+struct Initial {
+  pub camera_position: Point3<f32>,
+  pub look_direction: Point3<f32>,
+  pub up_direction: Vector3<f32>,
+
+  pub animation_speed: f32,
+}
+
+struct State {
+  pipeline: wgpu::RenderPipeline,
+
+  shapes: Shapes,
+
+  vert_bind_group: wgpu::BindGroup,
+  //frag_bind_group: wgpu::BindGroup,
+  vp_uniform_buffer: wgpu::Buffer,
+
+  msaa_texture_view: wgpu::TextureView,
+  depth_texture_view: wgpu::TextureView,
+
+  view_mat: Matrix4<f32>,
+  project_mat: Matrix4<f32>,
+
+  animation_speed: f32,
+}
+
+impl<'a> Render<'a> for State {
+  type Initial = Initial;
+
+  async fn new(ctx: &WgpuContext<'a>, initial: &Self::Initial) -> Self {
+    //
+    // shader
+    //
+
+    let vs_shader = ctx
+      .device
+      .create_shader_module(wgpu::include_wgsl!("./shader-vert.wgsl"));
+    let fs_shader = ctx
+      .device
+      .create_shader_module(wgpu::include_wgsl!("./shader-frag.wgsl"));
+
+    //
+    // matrix
+    //
+
+    let objects_count = NUM_CUBES + NUM_SPHERES + NUM_TORI;
+    let aspect = ctx.size.width as f32 / ctx.size.height as f32;
+
+    let Matrices {
+      model_mat,
+      normal_mat,
+      color_vec,
+    } = instance_defs::create_transform_mat_color(objects_count, true);
+
+    let view_mat = wt::create_view_mat(
+      initial.camera_position,
+      initial.look_direction,
+      initial.up_direction,
+    );
+    let project_mat = wt::create_projection_mat(aspect, true);
+    let vp_mat = project_mat * view_mat;
+
+    //
+    // uniform
+    //
+
+    let vp_uniform_buffer =
+      ctx.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("View-Projection Buffer"),
+        contents: cast_slice(vp_mat.as_ref() as &[f32; 16]),
+        usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+      });
+
+    let model_uniform_buffer =
+      ctx.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Model Uniform Buffer"),
+        contents: cast_slice(model_mat.as_slice()),
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+      });
+
+    let normal_uniform_buffer =
+      ctx.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Normal Uniform Buffer"),
+        contents: cast_slice(normal_mat.as_slice()),
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+      });
+
+    let color_uniform_buffer =
+      ctx.device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("color Uniform Buffer"),
+        contents: cast_slice(color_vec.as_slice()),
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+      });
+
+    // TODO: light uniform buffer
+    // TODO: material uniform buffer
+
+    //
+    // uniform bind group for vertex shader
+    //
+
+    let vert_bind_group_layout = helper_util::create_bind_group_layout(
+      &ctx.device,
+      &[
+        wgpu::BufferBindingType::Uniform,
+        wgpu::BufferBindingType::Storage { read_only: true },
+        wgpu::BufferBindingType::Storage { read_only: true },
+        wgpu::BufferBindingType::Storage { read_only: true },
+      ],
+      &[
+        wgpu::ShaderStages::VERTEX,
+        wgpu::ShaderStages::VERTEX,
+        wgpu::ShaderStages::VERTEX,
+        wgpu::ShaderStages::VERTEX,
+      ],
+    );
+
+    let vert_bind_group = helper_util::create_bind_group(
+      &ctx.device,
+      &vert_bind_group_layout,
+      &[
+        vp_uniform_buffer.as_entire_binding(),
+        model_uniform_buffer.as_entire_binding(),
+        normal_uniform_buffer.as_entire_binding(),
+        color_uniform_buffer.as_entire_binding(),
+      ],
+    );
+
+    //
+    // TODO: uniform bind group for fragment shader
+    //
+
+    //
+    // pipeline
+    //
+
+    let vertex_buffer_layout = wgpu::VertexBufferLayout {
+      array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+      step_mode: wgpu::VertexStepMode::Vertex,
+      attributes: &wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3],
+    };
+
+    let pipeline_layout =
+      ctx.device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[
+          &vert_bind_group_layout,
+          //&frag_bind_group_layout
+        ],
+        push_constant_ranges: &[],
+      });
+
+    let mut render = helper_util::RenderSet {
+      vs_shader: Some(&vs_shader),
+      fs_shader: Some(&fs_shader),
+      pipeline_layout: Some(&pipeline_layout),
+      vertex_buffer_layout: &[vertex_buffer_layout],
+      ..Default::default()
+    };
+    let pipeline = render.new(&ctx);
+
+    //
+    // texture views
+    //
+
+    let msaa_texture_view = helper_util::create_msaa_texture_view(&ctx);
+    let depth_texture_view = helper_util::create_depth_view(&ctx);
+
+    //
+    // vertex and index buffers for objects
+    //
+
+    let shapes = instance_defs::create_object_buffers(&ctx.device);
+
+    Self {
+      pipeline,
+      shapes,
+      vert_bind_group,
+      //frag_bind_group: None,
+      vp_uniform_buffer,
+      msaa_texture_view,
+      depth_texture_view,
+      view_mat,
+      project_mat,
+      animation_speed: initial.animation_speed,
+    }
+  }
+
+  fn resize(&mut self, ctx: &WgpuContext<'_>, size: Option<PhysicalSize<u32>>) {
+    let size = size.unwrap_or(ctx.size);
+
+    if size.width > 0 && size.height > 0 {
+      if let Some(surface) = &ctx.surface {
+        surface.configure(&ctx.device, &ctx.config.as_ref().unwrap());
+      }
+
+      self.project_mat =
+        wt::create_projection_mat(size.width as f32 / size.height as f32, true);
+
+      self.depth_texture_view = helper_util::create_depth_view(ctx);
+
+      if ctx.sample_count > 1 {
+        self.msaa_texture_view = helper_util::create_msaa_texture_view(&ctx);
+      }
+    }
+  }
+
+  fn update(&mut self, ctx: &WgpuContext, _dt: std::time::Duration) {
+    //let dt = self.animation_speed * dt.as_secs_f32();
+    //let sin = 10.0 * (0.5 + dt.sin());
+    //let cos = 10.0 * (0.5 + dt.cos());
+
+    // TODO: update light
+
+    // let view_project_mat = self.project_mat * self.view_mat;
+    // let view_projection_ref: &[f32; 16] = view_project_mat.as_ref();
+    // ctx.queue.write_buffer(
+    //   &self.vp_uniform_buffer,
+    //   0,
+    //   cast_slice(view_projection_ref),
+    // );
+
+    // TODO: update material
+  }
+
+  fn draw(
+    &mut self,
+    encoder: &mut wgpu::CommandEncoder,
+    target: RenderTarget,
+    sample_count: u32,
+  ) -> anyhow::Result<Option<wgpu::SurfaceTexture>, wgpu::SurfaceError> {
+    let (view, frame) = match target {
+      RenderTarget::Surface(surface) => {
+        let frame = surface.get_current_texture()?;
+        let view =
+          frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        (view, Some(frame))
+      }
+      RenderTarget::Texture(texture) => {
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        (view, None)
+      }
+    };
+
+    let color_attach = helper_util::create_color_attachment(&view);
+    let msaa_attach =
+      helper_util::create_msaa_color_attachment(&view, &self.msaa_texture_view);
+    let color_attachment = if sample_count == 1 {
+      color_attach
+    } else {
+      msaa_attach
+    };
+    let depth_attachment =
+      helper_util::create_depth_stencil_attachment(&self.depth_texture_view);
+
+    let mut render_pass =
+      encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: Some("Render Pass"),
+        color_attachments: &[Some(color_attachment)],
+        depth_stencil_attachment: Some(depth_attachment),
+        ..Default::default()
+      });
+
+    render_pass.set_pipeline(&self.pipeline);
+    render_pass.set_bind_group(0, &self.vert_bind_group, &[]);
+    //render_pass.set_bind_group(1, &self.frag_bind_group, &[]);
+
+    // draw cubes
+    render_pass.set_vertex_buffer(0, self.shapes.cube.vertex_buffer.slice(..));
+    render_pass.set_index_buffer(
+      self.shapes.cube.index_buffer.slice(..),
+      wgpu::IndexFormat::Uint16,
+    );
+    render_pass.draw_indexed(0..self.shapes.cube.index_count, 0, 0..NUM_CUBES);
+
+    // draw spheres
+    render_pass
+      .set_vertex_buffer(0, self.shapes.sphere.vertex_buffer.slice(..));
+    render_pass.set_index_buffer(
+      self.shapes.sphere.index_buffer.slice(..),
+      wgpu::IndexFormat::Uint16,
+    );
+    render_pass.draw_indexed(
+      0..self.shapes.sphere.index_count,
+      0,
+      NUM_CUBES..NUM_CUBES + NUM_SPHERES,
+    );
+
+    // draw tori
+    render_pass.set_vertex_buffer(0, self.shapes.torus.vertex_buffer.slice(..));
+    render_pass.set_index_buffer(
+      self.shapes.torus.index_buffer.slice(..),
+      wgpu::IndexFormat::Uint16,
+    );
+    render_pass.draw_indexed(
+      0..self.shapes.torus.index_count,
+      0,
+      NUM_CUBES + NUM_SPHERES..NUM_CUBES + NUM_SPHERES + NUM_TORI,
+    );
+
+    drop(render_pass);
+
+    Ok(frame)
+  }
 }
